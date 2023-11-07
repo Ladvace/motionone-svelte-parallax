9564b605c9667771a666b6f822e0df442049a460
Gianmarco <cavallogianmarco@gmail.com>
2023-11-07 11:20:04 +0100

    some twicking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import "../style.css";
   import { onMount } from "svelte";
-  import { animate, scroll, timeline } from "motion";
+  import { ScrollOffset, animate, scroll, timeline } from "motion";
   import sky from "$lib/assets/sky.png";
   import mid from "$lib/assets/mid.png";
   import fore from "$lib/assets/fore.png";
@@ -19,16 +19,16 @@
   ];
 
   onMount(() => {
-    const container = document.getElementById("main-container");
+    const container = document.getElementById("header-container");
     if (!container) return;
 
     const height = container.offsetHeight / 2;
 
     const sequence = [
       [
-        ".depth-sky",
+        ".depth-fore",
         {
-          transform: `translateY(${-(height * 0.35) - height * 0.05}px)`,
+          transform: `translateY(${-(height * 0.45)}px)`,
           ease: "none",
         },
         {
@@ -38,17 +38,18 @@
       [
         ".depth-mid",
         {
-          transform: `translateY(${-(height * 0.25)}px)`,
+          transform: `translateY(${-(height * 0.35)}px)`,
           ease: "none",
         },
         {
           transform: `translateY(0)`,
         },
+        { at: "<" },
       ],
       [
-        ".depth-fore",
+        ".depth-sky",
         {
-          transform: `translateY(${-(height * 0.35)}px)`,
+          transform: `translateY(${-(height * 0.25)}px)`,
           ease: "none",
         },
         {
@@ -58,43 +59,37 @@
     ];
 
     images.forEach((image) => {
-      let translateY: number = -(height * 0.35) - height * 0.05;
-
-      switch (image.depth) {
-        case ParallaxDepth.sky:
-          translateY = -(height * 0.35) - height * 0.05;
-          break;
-        case ParallaxDepth.mid:
-          translateY = -(height * 0.25);
-          break;
-        case ParallaxDepth.fore:
-          translateY = -(height * 0.35);
-          break;
-      }
-
       scroll(timeline(sequence as any), {
         target: container,
-        offset: ["center center", "end end"],
+        offset: ["start start", "end end"],
       });
     });
   });
 </script>
 
-<main
-  id="main-container"
-  class="h-[300vh] w-screen overflow-hidden bg-white relative"
->
-  {#each images as image}
-    <div
-      class={`image-container depth-${image.depth}`}
-      style={`z-index: ${image.zIndex};`}
-    >
-      <img src={image.src} alt={`img ${image.depth}`} class="w-full h-full" />
-    </div>
-  {/each}
+<main>
+  <header
+    id="header-container"
+    class="h-screen xl:h-[200vh] w-screen overflow-hidden bg-white relative"
+  >
+    {#each images as image}
+      <div
+        class={`image-container depth-${image.depth}`}
+        style={`z-index: ${image.zIndex};`}
+      >
+        <img src={image.src} alt={`img ${image.depth}`} class="w-full h-full" />
+      </div>
+    {/each}
+    <div id="fade" />
+  </header>
+  <div class="h-screen bg-black" />
 </main>
 
 <style>
+  :global(*) {
+    overscroll-behavior-y: none;
+  }
+
   .image-container {
     position: absolute;
     width: 100%;
@@ -106,5 +101,31 @@
   .image-container.depth-sky {
     top: 0;
     bottom: initial;
+  }
+
+  #fade {
+    position: absolute;
+    height: 50vh;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: linear-gradient(
+      to top,
+      black 0%,
+      rgba(0, 0, 0, 0.738) 19%,
+      rgba(0, 0, 0, 0.541) 34%,
+      rgba(0, 0, 0, 0.382) 47%,
+      rgba(0, 0, 0, 0.278) 56.5%,
+      rgba(0, 0, 0, 0.194) 65%,
+      rgba(0, 0, 0, 0.126) 73%,
+      rgba(0, 0, 0, 0.075) 80.2%,
+      rgba(0, 0, 0, 0.042) 86.1%,
+      rgba(0, 0, 0, 0.021) 91%,
+      rgba(0, 0, 0, 0.008) 95.2%,
+      rgba(0, 0, 0, 0.002) 98.2%,
+      transparent 100%
+    );
+    z-index: 10;
   }
 </style>
